fa3250aee7a49fec8102078cbffdcda849a80081
SandraLe <lechtenberg.sandra@gmail.com>
2015-05-23 16:53:03 +0200

    -test

---
+++ src/main/java/weplacm/WeplacmApplication.java
@@ -0,0 +1,9 @@
+package weplacm;
+ 
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+ 
+@ProcessApplication("Loan Approval App")
+public class WeplacmApplication extends ServletProcessApplication {
+  // empty implementation
+}
