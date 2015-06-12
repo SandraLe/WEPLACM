adc894e80bb0b793da0cd134d0a0b67841000461
piadiedam <pia.diedam@gmail.com>
2015-06-12 23:37:35 +0200

    Additional Implementation + Anpassung als EJB Projekt: Läuft nicht mehr auf Tomcat sondern nur noch auf JBoss oder Wildfly

---
+++ src/main/java/weplacm/MandateEntity.java
@@ -0,0 +1,42 @@
+package weplacm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class MandateEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L; 
+	
+	@Id
+	@GeneratedValue
+	protected Long id; 
+	
+	@Version
+	protected Long version; 
+	
+	protected Long job_profile_id; 
+	protected Long candidate_list_id; 
+	
+	public Long getJob_profile_id() {
+		return job_profile_id;
+	}
+	public void setJob_profile_id(Long job_profile_id) {
+		this.job_profile_id = job_profile_id;
+	}
+	public Long getCandidate_list_id() {
+		return candidate_list_id;
+	}
+	public void setCandidate_list_id(Long candidate_list_id) {
+		this.candidate_list_id = candidate_list_id;
+	}
+	
+	
+
+}
--- src/main/java/weplacm/WeplacmApplication.java
+++
@@ -1,9 +0,0 @@
-package weplacm;
- 
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
- 
-@ProcessApplication("WEPLACM")
-public class WeplacmApplication extends ServletProcessApplication {
-  // empty implementation
-}
